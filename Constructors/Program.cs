de1f995b9a7e32a8217eb534cabe83d3b900d092
Nedroid <nesekrmn02@gmail.com>
2024-05-10 02:46:36 +0300

    ders4odev2Constructors added

---
+++ Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//default constructor
+            Customer customer = new Customer { Id = 1, FirstName = "Engin", LastName = "demirog", City = "Ankara" };
+            //override parametreli constructor 
+            Customer customer2 = new Customer(2, "Derin", "demirog", "bursa");
+            Console.ReadLine();
+
+            
+
+        }
+    }
+
+    class Customer
+    {
+
+        //Default Constructor
+        public Customer()
+        {
+            Console.WriteLine("Yapıcı blok Çalıştı");
+          
+        }
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id { get; set; }
+        public string FirstName{ get; set; }
+        
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
