98bc5e9505f485bcc833706cb5dca4f06f971126
nese <nesekrmn02@gmail.com>
2024-06-01 19:38:51 +0300

    ders 4 odev 5 with error

---
+++ ders4odev5MyDictionary/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ders4odev5MyDictionary
+{
+    public class MyDictionary<Key, Value>
+
+    {
+        List<Key> keys;
+        List<Value> values;
+        public MyDictionary()
+        {
+            keys = new List<Key>();
+            values = new List<Value>();
+        }
+        public void Add(Key key, Value value)
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+
+
+        public void Clear()
+        {
+            keys.Clear();
+            values.Clear();
+        }
+        public List<Key> GetKeys() { return keys; }
+        public List<Value> GetValues() { return values; }
+
+    }
+}
---
+++ ders4odev5MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ders4odev5MyDictionary;
+
+Console.WriteLine("Hello, World!");
+MyDictionary<int,string> myCity = new MyDictionary<int,string>();
+myCity.Add(1, "Adana");
+myCity.Add(2, "adıyaman");
+myCity.Add(3, "antep");
+myCity.Add(4, "Ankara");
+myCity.Add(5, "bursa");
+List<int> key=myCity.GetKeys();
+List<string>value=myCity.GetValues();
+
+for (int i = 0; i < key.Count; i++) 
+{
+    Console.WriteLine("City="+key+"\n city number"+value);
+}
+Console.ReadLine();
